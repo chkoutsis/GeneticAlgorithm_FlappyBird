06951de2542229c601ae3d46e33262771df29614
Charis <xariskoutsis@gmail.com>
2024-02-22 01:00:52 +0200

    Updated hidden layers

--- genetic_algorithms/model.py
+++ genetic_algorithms/model.py
@@ -9,9 +9,11 @@ def dna():
     - model (tf.keras.Model): A TensorFlow Keras model.
     """
     input_layer = tf.keras.Input((6), name='Input')
-    dense_layer = tf.keras.layers.Dense(
-        10, activation="relu", name='Hidden')(input_layer)
+    hidden_1_layer = tf.keras.layers.Dense(
+        32, activation="relu", name='Hidden_1')(input_layer)
+    hidden_2_layer = tf.keras.layers.Dense(
+        64, activation="relu", name='Hidden_2')(hidden_1_layer)
     output_layer = tf.keras.layers.Dense(
-        1, activation="linear", name='Output')(dense_layer)
+        1, activation="linear", name='Output')(hidden_2_layer)
     model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
     return model
